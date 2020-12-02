f005e05178541707f6731cd741b1b525ed97dbd9
federalolive <j3pumphrey@gmail.com>
2020-12-01 19:59:03 -0600

    Add routes callback function

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 // Middleware
 
 // Routes
-app.listen(3000, function() {
+app.get('/', function(req, res) {
+    res.send('<h1>Airports</h1>');
+});
+
+app.listen(3000, function () {
     console.log('Listening on port 3000');
   });
