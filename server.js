3ac9ef3073d318e0e8e74c05a10fe8cfc86248ec
federalolive <j3pumphrey@gmail.com>
2020-12-01 19:49:07 -0600

    Complete basic strcture of app

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Modules
+const express = require('express');
 
 // Express App
+const app = express();
 
 // Configure App
 
 // Middleware
 
 // Routes
-
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
